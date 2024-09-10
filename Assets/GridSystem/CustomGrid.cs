d2d5e43ab05157fc1cf84111770fa266e6454d80
Dylan Worseling <dylan-dylan99@hotmail.com>
2024-09-10 17:03:18 +0200

    feat: BFS (WIP)

--- Assets/GridSystem/CustomGrid.cs
+++ Assets/GridSystem/CustomGrid.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CustomGrid : MonoBehaviour
@@ -10,6 +13,29 @@ public class CustomGrid : MonoBehaviour
     private Vector3 originPos;
     private int[,] gridArray;
 
+    private bool[,] rightWallArray;
+    private bool[,] bottomWallArray;
+    private Dictionary<Vector2Int, TextMesh> textMeshes = new Dictionary<Vector2Int, TextMesh>();
+
+    // Direction enum to represent movement directions
+    public enum Direction
+    {
+        NORTH, EAST, SOUTH, WEST
+    }
+
+    private static readonly Dictionary<Direction, Vector2Int> DirectionVectors = new Dictionary<Direction, Vector2Int>
+    {
+        { Direction.EAST, new Vector2Int(1, 0) },
+        { Direction.WEST, new Vector2Int(-1, 0) },
+        { Direction.NORTH, new Vector2Int(0, 1) },
+        { Direction.SOUTH, new Vector2Int(0, -1) },
+    };
+
+    private static readonly int NUM_DIRECTIONS = Enum.GetValues(typeof(Direction)).Length;
+    private static readonly int[] DELTA_X = { 0, +1, 0, -1 };
+    private static readonly int[] DELTA_Y = { -1, 0, +1, 0 };
+
+
     public CustomGrid(int width, int height, int cellSize, Vector3 originPos)
     {
         this.width = width;
@@ -19,19 +45,37 @@ public class CustomGrid : MonoBehaviour
 
         gridArray = new int[width, height];
 
-        for (int x = 0; x < gridArray.GetLength(0); x++)
+        rightWallArray = new bool[width, height + 1];
+        bottomWallArray = new bool[width + 1, height];
+       
+
+        for (int x = 0; x < width; x++)
         {
-            for (int z = 0; z < gridArray.GetLength(1); z++)
+            for (int z = 0; z < height; z++)
             {
-                //Debug.DrawLine(GetWorldPos(x, z), GetWorldPos(x + 1, z), Color.white, 1000f);
-                //Debug.DrawLine(GetWorldPos(x, z), GetWorldPos(x, z + 1), Color.white, 1000f);
+                rightWallArray[x, z] = false;
+                bottomWallArray[x, z] = false;
             }
         }
+
+        //for (int x = 0; x < gridArray.GetLength(0); x++)
+        //{
+        //    for (int z = 0; z < gridArray.GetLength(1); z++)
+        //    {
+        //        //Debug.DrawLine(GetWorldPos(x, z), GetWorldPos(x + 1, z), Color.white, 1000f);
+        //        //Debug.DrawLine(GetWorldPos(x, z), GetWorldPos(x, z + 1), Color.white, 1000f);
+        //    }
+        //}
     }
 
     public int GetCellSize()
     {
         return cellSize;
+    }
+
+    public int cellNumber(int x, int z)
+    {
+        return x + z * this.width;
     }
 
     public Vector3 GetWorldPos(int x, int z)
@@ -46,5 +90,238 @@ public class CustomGrid : MonoBehaviour
         return new int[] { x, z };
 
     }
+
+    public void AddTextMesh(int x, int z, TextMesh textMesh)
+    {
+        Vector2Int coords = new Vector2Int(x, z);
+        textMeshes[coords] = textMesh;
+    }
+
+    public void UpdateTextColor(Vector2Int coords, Color color)
+    {
+        if (textMeshes.ContainsKey(coords))
+        {
+            textMeshes[coords].color = color;
+        }
+    }
+
+    public bool GetWall(int x, int z, Direction direction)
+    {
+        switch(direction)
+        {
+            case Direction.NORTH:
+                return bottomWallArray[x, z];
+                case Direction.EAST:
+                return rightWallArray[x, z + 1];
+                case Direction.SOUTH:
+                return bottomWallArray[x + 1, z];
+                case Direction.WEST:
+                return rightWallArray[x, z];
+        }
+        return false;
+    }
+
+    public void SetWall(int x, int z, Direction direction, bool value)
+    {
+        switch (direction)
+        {
+            case Direction.NORTH:
+                bottomWallArray[x, z] = value;
+                break;
+            case Direction.EAST:
+                rightWallArray[x, z + 1] = value;
+                break;
+            case Direction.SOUTH:
+                bottomWallArray[x + 1, z] = value;
+                break;
+            case Direction.WEST:
+                rightWallArray[x, z] = value;
+                break;
+        }
+    }
+
+
+    public int[,] GetGrid()
+    {
+        return gridArray;
+    }
+
+
+    private bool IsValidPosition(Vector2Int position)
+    {
+        return position.x >= 0 && position.x < width && position.y >= 0 && position.y < height;
+    }
+
+    private bool HasWallBetween(Vector2Int cellA, Vector2Int cellB, Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.EAST:
+                return GetWall(cellA.x, cellA.y, Direction.EAST);
+            case Direction.WEST:
+                return GetWall(cellB.x, cellB.y + 1, Direction.WEST);
+            case Direction.NORTH:
+                return GetWall(cellA.x + 1, cellA.y, Direction.NORTH);
+            case Direction.SOUTH:
+                return GetWall(cellB.x, cellB.y, Direction.SOUTH);
+            default:
+                return false;
+        }
+    }
+
+    private Direction GetPerpendicularDirection(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.EAST: return Direction.NORTH;
+            case Direction.WEST: return Direction.SOUTH;
+            case Direction.NORTH: return Direction.EAST;
+            case Direction.SOUTH: return Direction.WEST;
+            default: return Direction.EAST; // Default value
+        }
+    }
+
+    private int GetNumWalls(Vector2Int cell)
+    {
+        int numWalls = 0;
+        foreach (Direction dir in Enum.GetValues(typeof(Direction)))
+        {
+            numWalls += GetWall(cell.x, cell.y, dir) ? 1 : 0;
+        }
+
+        return numWalls;
+    }
+
+    private int GetDirectNeighbor(Vector2Int cell, Direction dir)
+    {
+        int neighborX = cell.x + DELTA_X[(int)dir];
+        int neighborY = cell.y + DELTA_Y[(int)dir];
+
+        if (neighborX < 0 || neighborX >= this.width) return -1;
+        if (neighborY < 0 || neighborY >= this.height) return -1;
+
+        // compute neighbour node number
+        return cellNumber(neighborX, neighborY);
+    }
+
+
+    public HashSet<Vector2Int> GetNeighbors(Vector2Int fromVertex)
+    {
+        var neighbors = new HashSet<Vector2Int>();
+
+        foreach (var direction in DirectionVectors)
+        {
+            Vector2Int directionVec = direction.Value;
+            Vector2Int nextNeighbor = fromVertex + directionVec;
+
+            if (IsValidPosition(nextNeighbor) && !HasWallBetween(fromVertex, nextNeighbor, direction.Key))
+            {
+                // Follow the passage along the direction
+                Vector2Int neighbor = nextNeighbor;
+                int numWalls = GetNumWalls(neighbor);
+                Vector2Int nextNeighborAlongDirection;
+
+                // Continue following in the same direction if possible
+                do
+                {
+                    neighbor = nextNeighbor;
+                    numWalls = GetNumWalls(neighbor);
+                    nextNeighborAlongDirection = neighbor + directionVec;
+                    nextNeighbor = nextNeighborAlongDirection;
+                }
+                while (IsValidPosition(nextNeighbor) &&
+                       !HasWallBetween(neighbor, nextNeighbor, direction.Key) &&
+                       numWalls == 2); // Adjust this to match the requirement of having exactly 2 walls
+
+                // Debug log
+                Debug.Log($"Adding neighbor {neighbor} for fromVertex {fromVertex}");
+
+                // Try to continue around a corner if the path follows the constraints
+                if (numWalls == 2)
+                {
+                    Direction turnedDirection = GetPerpendicularDirection(direction.Key);
+                    nextNeighbor = neighbor + DirectionVectors[turnedDirection];
+
+                    while (IsValidPosition(nextNeighbor) &&
+                           !HasWallBetween(neighbor, nextNeighbor, turnedDirection) &&
+                           numWalls == 2)
+                    {
+                        do
+                        {
+                            neighbor = nextNeighbor;
+                            numWalls = GetNumWalls(neighbor);
+                            nextNeighbor = neighbor + DirectionVectors[turnedDirection];
+                        }
+                        while (IsValidPosition(nextNeighbor) &&
+                               !HasWallBetween(neighbor, nextNeighbor, turnedDirection) &&
+                               numWalls == 2);
+
+                        // Debug log
+                        Debug.Log($"Continuing around the corner, adding neighbor {neighbor}");
+
+                        // Try again around the original direction
+                        nextNeighbor = neighbor + directionVec;
+                        while (IsValidPosition(nextNeighbor) &&
+                               !HasWallBetween(neighbor, nextNeighbor, direction.Key) &&
+                               numWalls == 2)
+                        {
+                            neighbor = nextNeighbor;
+                            numWalls = GetNumWalls(neighbor);
+                            nextNeighbor = neighbor + directionVec;
+                        }
+
+                        nextNeighbor = neighbor + DirectionVectors[turnedDirection];
+                    }
+                }
+
+                neighbors.Add(neighbor);
+            }
+        }
+
+        // Debug log
+        Debug.Log($"Neighbors for fromVertex {fromVertex}: {string.Join(", ", neighbors.Select(n => $"({n.x}, {n.y})"))}");
+
+        return neighbors;
+    }
+
+
+
+
+    //// Check neighbors for solvability (used in DFS/BFS)
+    //public List<Vector2Int> GetNeighbors(int x, int z)
+    //{
+    //    List<Vector2Int> neighbors = new List<Vector2Int>();
+
+    //    // Check left neighbor (x - 1, z)
+    //    if (x > 0 && !HasRightWall(x - 1, z)) // Check left neighbor's right wall
+    //    {
+    //        Debug.Log("Left");
+    //        neighbors.Add(new Vector2Int(x - 1, z));
+    //    }
+
+    //    // Check right neighbor (x + 1, z)
+    //    if (x < width - 1 && !HasRightWall(x, z)) // Check current cell's right wall
+    //    {
+    //        Debug.Log("Right");
+    //        neighbors.Add(new Vector2Int(x + 1, z));
+    //    }
+
+    //    // Check bottom neighbor (x, z - 1)
+    //    if (z > 0 && !HasBottomWall(x, z)) // Check current cell's bottom wall
+    //    {
+    //        Debug.Log("Bottom");
+    //        neighbors.Add(new Vector2Int(x, z - 1));
+    //    }
+
+    //    // Check top neighbor (x, z + 1)
+    //    if (z < height - 1 && !HasBottomWall(x, z)) // Check current cell's bottom wall
+    //    {
+    //        Debug.Log("Top");
+    //        neighbors.Add(new Vector2Int(x, z + 1));
+    //    }
+
+    //    return neighbors;
+    //}
+
 
 }
--- Assets/Maze/EllersMazeGenerator.cs
+++ Assets/Maze/EllersMazeGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -87,7 +88,6 @@ static class Extensions
         Renderer renderer = indicator.GetComponent<Renderer>();
         if (renderer)
         {
-            Debug.Log("color");
             renderer.material.color = color;
         }
     }
@@ -114,10 +114,12 @@ static class Extensions
 
             max_set_val += 1;
         }
+
         PlaceWallsSameDir(firstRow, Vector3.right);
 
         List<(int[], int)> row = firstRow;
         List<int[]> sameSetWalls = new List<int[]>();
+
         for (int z = 0; z < gridSpawner.height; z++)
         {
             if (z == gridSpawner.height - 1)
@@ -128,15 +130,30 @@ static class Extensions
             }
 
             var joinedRow = JoinRowVerticalWalls(row, sameSetWalls);
-            WorldTextRow(joinedRow, Color.red, 1);
-
             BottomWalls(joinedRow, out List<int[]> bWallLoc);
 
             row = IncreaseAxisRow(1, EmptyCellsNewRow(joinedRow, bWallLoc));
         }
 
+        VertexText(gridXZ.GetGrid(), Color.blue, 1);
+
         // Place start and end points with colored indicators
         PlaceStartAndEndPoints();
+
+        // After generating the maze, check if it is solvable and visualize
+        List<Vector2Int> path;
+        HashSet<Vector2Int> visitedNodes;
+
+        if (IsSolvable(startPoint, endPoint, out path, out visitedNodes))
+        {
+            Debug.Log("Maze is solvable!");
+
+        }
+        else
+        {
+            Debug.Log("Maze is unsolvable.");
+            // Handle unsolvable maze case if needed
+        }
     }
 
     private List<(int[], int)> JoinRowVerticalWalls(List<(int[], int)> row, List<int[]> sameSetWalls)
@@ -185,9 +202,23 @@ static class Extensions
                 }
             }
 
-            PlaceWallXZ(Vector3.forward, rowCopy[rowCopy.Count - 1].Item1[0] + 1, rowCopy[rowCopy.Count - 1].Item1[1]);
+        // This line needs to handle the outer boundary case correctly
+        int x = rowCopy[rowCopy.Count - 1].Item1[0] + 1;
+        int z = rowCopy[rowCopy.Count - 1].Item1[1];
 
-            return rowCopy;
+        // Ensure walls are placed at the outer boundary (max width/height)
+        if (x <= gridSpawner.width && z < gridSpawner.height)
+        {
+            PlaceWallXZ(Vector3.forward, x, z);
+        }
+
+        // Alternatively for right walls:
+        if (x < gridSpawner.width && z <= gridSpawner.height)
+        {
+            PlaceWallXZ(Vector3.right, x, z);
+        }
+
+        return rowCopy;
         }
 
         private bool InRangeInclusive(int num, int minRange, int maxRange)
@@ -323,11 +354,74 @@ static class Extensions
                 var coords = cellTup.Item1;
 
                 PlaceWallXZ(dir, coords[0], coords[1]);
+
+            // Ensure the neighboring cell also has the opposite wall
+            if (dir == Vector3.right) // Right wall
+            {
+                // Place left wall on the neighboring cell
+                if (coords[0] + 1 < gridSpawner.width)
+                {
+                    PlaceWallXZ(Vector3.right, coords[0] + 1, coords[1]);
+                }
             }
+            else if (dir == Vector3.forward) // Bottom wall
+            {
+                // Place top wall on the neighboring cell
+                if (coords[1] + 1 < gridSpawner.height)
+                {
+                    PlaceWallXZ(Vector3.forward, coords[0], coords[1] + 1);
+                }
+            }
+        }
+
         }
 
     private void PlaceWallXZ(Vector3 dir, int x, int z, Color? color = null)
     {
+        bool isOuterWall = (x == gridSpawner.width || z == gridSpawner.height || x == 0 || z == 0);
+
+        // Debug log to verify if it's identifying outer walls correctly
+        //Debug.Log($"Placing wall at ({x}, {z}) in direction {dir}. Is outer wall: {isOuterWall}");
+
+        if (!isOuterWall && (x < 0 || x >= gridSpawner.width || z < 0 || z >= gridSpawner.height))
+        {
+            Debug.LogWarning($"Wall placement out of bounds at ({x}, {z}) in direction {dir}");
+            return; // Skip if the coordinates are invalid and it's not an outer wall
+        }
+
+        // Update wall data for the current cell
+        if (dir == Vector3.right) // Right wall
+        {
+            if (x >= 0 && x < gridSpawner.width && z >= 0 && z < gridSpawner.height) // Ensure valid grid cell
+            {
+                gridXZ.SetWall(x, z, CustomGrid.Direction.WEST, true);
+                gridXZ.SetWall(x, z, CustomGrid.Direction.EAST, true);
+            }
+        }
+        else if (dir == Vector3.forward) // Bottom wall
+        {
+            if (x >= 0 && x < gridSpawner.width && z >= 0 && z < gridSpawner.height) // Ensure valid grid cell
+            {
+                gridXZ.SetWall(x, z, CustomGrid.Direction.NORTH, true);
+                gridXZ.SetWall(x, z, CustomGrid.Direction.SOUTH, true);
+            }
+        }
+        //else if (dir == Vector3.right) // Left wall (opposite direction)
+        //{
+        //    if (x - 1 >= 0 && x - 1 < gridSpawner.width && z >= 0 && z < gridSpawner.height) // Ensure valid grid cell
+        //    {
+        //        gridXZ.SetRightWall(x - 1, z, true);
+        //    }
+        //}
+        //else if (dir == Vector3.forward) // Top wall (opposite direction)
+        //{
+        //    if (x >= 0 && x < gridSpawner.width && z - 1 >= 0 && z - 1 < gridSpawner.height) // Ensure valid grid cell
+        //    {
+        //        gridXZ.SetBottomWall(x, z - 1, true);
+        //    }
+        //}
+
+        // Place the visual wall object
         Vector3 position = gridXZ.GetWorldPos(x, z);
         position.y = WallYValue;
 
@@ -335,54 +429,189 @@ static class Extensions
         Renderer renderer = wallObj.GetComponent<Renderer>();
         if (renderer != null)
         {
-            if (color != null)
-            {
-                renderer.material.color = color.Value;
-            } 
-            else
-            {
-                renderer.material.color = Color.cyan;
-            }
+            renderer.material.color = color ?? Color.cyan;
         }
     }
 
-    private void WorldTextRow(List<(int[], int)> row, Color textColor, int yVal)
+
+
+    private void VertexText(int[,] grid, Color textColor, int yVal)
     {
-        GameObject rowObj = new GameObject();
+        int counter = 1; // Initialize the counter
 
-        foreach (var cellTup in row)
+        // Create a new GameObject for the vertex text
+        GameObject gridTextObj = new GameObject("GridText");
+
+        // Iterate over each cell in the grid
+        for (int x = 0; x < grid.GetLength(0); x++)
         {
-            var coords = cellTup.Item1;
-            var set = cellTup.Item2;
-
-            // Determine the position and label text
-            string labelText;
-            if (coords[0] == startPoint.x && coords[1] == startPoint.y)
+            for (int y = 0; y < grid.GetLength(1); y++)
             {
-                labelText = "Start \n" + set.ToString();
-            }
-            else if (coords[0] == endPoint.x && coords[1] == endPoint.y)
-            {
-                labelText = "End \n" + set.ToString();
-            }
-            else
-            {
-                labelText = set.ToString();
-            }
+                // Determine the position and label text
+                string labelText = counter.ToString(); // Use the counter for unique numbers
 
-            // Create Text GameObject
-            var text = new GameObject();
-            var mesh = text.AddComponent<TextMesh>();
-            mesh.text = labelText;
-            mesh.color = textColor;
+                // Create Text GameObject
+                var text = new GameObject();
+                var mesh = text.AddComponent<TextMesh>();
+                mesh.text = labelText;
+                mesh.color = textColor;  // Color can be customized
 
-            Vector3 gridPos = gridXZ.GetWorldPos(coords[0], coords[1]);
-            gridPos = new Vector3(gridPos.x + gridXZ.GetCellSize() / 2, yVal, gridPos.z + gridXZ.GetCellSize() / 2);
-            text.transform.position = gridPos;
-            text.transform.parent = rowObj.transform;
+                // Calculate position in world space
+                Vector3 gridPos = gridXZ.GetWorldPos(x, y);
+                gridPos = new Vector3(gridPos.x + gridXZ.GetCellSize() / 2, yVal, gridPos.z + gridXZ.GetCellSize() / 2);
+                text.transform.position = gridPos;
+                text.transform.parent = gridTextObj.transform;
+
+                // Store reference to the TextMesh to allow updates later
+                gridXZ.AddTextMesh(x, y, mesh);
+
+                // Increment the counter for the next vertex
+                counter++;
+            }
         }
     }
 
+    private bool IsSolvable(Vector2Int start, Vector2Int end, out List<Vector2Int> path, out HashSet<Vector2Int> visitedNodes)
+    {
+        Queue<Vector2Int> frontier = new Queue<Vector2Int>();
+        visitedNodes = new HashSet<Vector2Int>(); // Track visited nodes
+        Dictionary<Vector2Int, Vector2Int> cameFrom = new Dictionary<Vector2Int, Vector2Int>(); // To reconstruct the path
+        frontier.Enqueue(start);
+        visitedNodes.Add(start);
+        cameFrom[start] = start;
+
+        Debug.Log($"Starting BFS from {start} to {end}");
+
+        gridXZ.UpdateTextColor(start, Color.red); // Mark the start point as red when visited
+
+        while (frontier.Count > 0)
+        {
+            Vector2Int current = frontier.Dequeue();
+
+            // If we reached the end, reconstruct the path
+            if (current == end)
+            {
+                path = ReconstructPath(cameFrom, start, end);
+                Debug.Log("Path found!");
+                VisualizePath(path, Color.green); // Use the VisualizePath method to show the path
+                return true;
+            }
+
+            Debug.Log($"{current.x} {current.y}");
+
+            // Get walkable neighbors
+            foreach (Vector2Int neighbor in GetWalkableNeighbors(current))
+            {
+                if (!visitedNodes.Contains(neighbor))
+                {
+                    frontier.Enqueue(neighbor);
+                    visitedNodes.Add(neighbor);
+                    cameFrom[neighbor] = current;
+
+                    // Mark the visited node as red
+                    gridXZ.UpdateTextColor(neighbor, Color.red);
+                }
+            }
+        }
+
+        // If no path is found
+        path = null;
+        Debug.Log("No path found.");
+        return false;
+    }
+
+
+
+    private List<Vector2Int> ReconstructPath(Dictionary<Vector2Int, Vector2Int> cameFrom, Vector2Int start, Vector2Int end)
+    {
+        List<Vector2Int> path = new List<Vector2Int>();
+        Vector2Int current = end;
+
+        while (current != start)
+        {
+            path.Add(current);
+            current = cameFrom[current];
+        }
+
+        path.Add(start); // Add start at the end
+        path.Reverse(); // Reverse to get path from start to end
+        return path;
+    }
+
+    private List<Vector2Int> GetWalkableNeighbors(Vector2Int current)
+    {
+        // Get neighbors from gridXZ as a HashSet
+        HashSet<Vector2Int> neighborSet = gridXZ.GetNeighbors(current);
+
+        // Convert HashSet to List
+        List<Vector2Int> neighbors = new List<Vector2Int>(neighborSet);
+
+
+        // Define the possible neighbor directions and their respective wall checks
+        Vector2Int[] directions = {
+        new Vector2Int(1, 0), // Right
+        new Vector2Int(-1, 0), // Left
+        new Vector2Int(0, 1), // Up
+        new Vector2Int(0, -1) // Down
+    };
+
+        foreach (var dir in directions)
+        {
+            Vector2Int neighbor = current + dir;
+            if (IsWithinBounds(neighbor) && CanMoveTo(current, dir))
+            {
+                neighbors.Add(neighbor);
+            }
+        }
+
+        return neighbors;
+    }
+
+    private bool IsWithinBounds(Vector2Int pos)
+    {
+        return pos.x >= 0 && pos.x < gridSpawner.width && pos.y >= 0 && pos.y < gridSpawner.height;
+    }
+
+    private bool CanMoveTo(Vector2Int current, Vector2Int direction)
+    {
+        Vector2Int right = new Vector2Int(1, 0);
+        Vector2Int left = new Vector2Int(-1, 0);
+        Vector2Int up = new Vector2Int(0, 1);
+        Vector2Int down = new Vector2Int(0, -1);
+
+        // Check if moving right
+        if (direction.Equals(right))
+        {
+            return current.x < gridSpawner.width - 1 && !gridXZ.GetWall(current.x, current.y, CustomGrid.Direction.EAST);
+        }
+        // Check if moving left
+        else if (direction.Equals(left))
+        {
+            return current.x > 0 && !gridXZ.GetWall(current.x - 1, current.y, CustomGrid.Direction.WEST);
+        }
+        // Check if moving up
+        else if (direction.Equals(up))
+        {
+            return current.y < gridSpawner.height - 1 && !gridXZ.GetWall(current.x, current.y, CustomGrid.Direction.NORTH);
+        }
+        // Check if moving down
+        else if (direction.Equals(down))
+        {
+            return current.y > 0 && !gridXZ.GetWall(current.x, current.y - 1, CustomGrid.Direction.SOUTH);
+        }
+
+        return false;
+    }
+
+    private void VisualizePath(List<Vector2Int> path, Color color)
+    {
+        foreach (var node in path)
+        {
+            PlaceGridCellIndicator(node, color); // Green for path, for example
+        }
+    }
 }
+
+
 
 #endregion
